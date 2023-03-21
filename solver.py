c9d4ef0fe755d31803172dd87022e1e5fc027d40
Jakub Pawlak <j.pawlak@student.fontys.nl>
2023-03-21 14:33:49 +0100

    Possibly working shortest paths finding

--- solver.py
+++ solver.py
@@ -1,4 +1,5 @@
 from board import Board
+import sys
 
 
 class Solver:
@@ -8,8 +9,24 @@ class Solver:
         # init Z table
 
     # minimum distance of getting from node A to the target within cost T
-    def z(self, a, t: int):
+    def z(self, a, t: int) -> int:
         pass
 
+    # set of the shortest paths from node A to the target within a budget T
     def shortest_paths(self, a, t: int):
-        pass
+        current_distance = self.z(a, t)
+        if a == self.board.peking:
+            yield [a]
+            return
+
+        if current_distance == sys.maxsize or t <= 0:
+            return
+
+        for b in self.board.graph.adj[a]:
+            new_budget = t - self.board.graph.edges[a, b]['cost']
+            if new_budget < 0:  # cannot afford
+                continue
+            new_distance = self.z(b, new_budget)
+            if new_distance < current_distance:
+                for p in self.shortest_paths(b, new_budget):
+                    yield [a] + p
