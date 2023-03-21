421c4a00c1d26e99a5c9fade21ddf20e32f13d97
Jakub Pawlak <j.pawlak@student.fontys.nl>
2023-03-21 13:43:10 +0100

    Add Solver class

---
+++ solver.py
@@ -0,0 +1,15 @@
+from board import Board
+
+
+class Solver:
+
+    def __init__(self, board: Board):
+        self.board = board
+        # init Z table
+
+    # minimum distance of getting from node A to the target within cost T
+    def z(self, a, t: int):
+        pass
+
+    def shortest_paths(self, a, t: int):
+        pass
