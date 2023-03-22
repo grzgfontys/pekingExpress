649f183ae0c7fdcfa9f5271d687ad927ed81e35a
Jakub Pawlak <j.pawlak@student.fontys.nl>
2023-03-22 21:46:37 +0100

    Added comparing 2 paths in terms of blocking

--- solver.py
+++ solver.py
@@ -28,6 +28,20 @@ class Solver:
             for p in self.shortest_paths(b, new_budget):
                 yield [a] + p
 
+    # compares paths a and b with regard to blocking
+    # returns 0 if there are no blocks,
+    # positive value if a blocks b
+    # and negative value if b blocks a
+    def blocking_cmp(self, a, b):
+        critical_locations = self.board.critical_locations
+        for i in range(len(a)):
+            if a[i] in critical_locations:
+                if a[i] == b[i]:
+                    return 1
+                elif i > 0 and a[i] == b[i - 1]:
+                    return -1
+        return 0
+
     def viable_moves(self, a, t: int):
         for b in self.board.graph.adj[a]:
             current_distance = self.z(a, t)
